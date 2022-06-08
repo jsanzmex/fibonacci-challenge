aaad2653f48edb18eb0ae471b842c7e68fab89a3
Julio Sánchez <julio.sanchez@sopristec.com>
2022-06-08 12:31:40 -0500

    + Prepends client env variables with "VITE_" so our code can access those variables in runtime. + Adds axios to client packages so we can communicate with our rails backend. + Creates the FibonacciForm class component to replace the main App component. + Finishes configuring RackCors to temporarily allow all the origins.

---
+++ services/client/src/FibonacciForm.jsx
@@ -0,0 +1,51 @@
+import { React, Component } from 'react'
+import axios from 'axios'
+
+class FibonacciForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {positiveResponseReceived: false, value: '', fibonacci: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('A value was submitted: ' + this.state.value);
+    const parsed = parseInt(this.state.value, 10);
+    this.setState({positiveResponseReceived: true});
+    this.getFibonacci(parsed);
+    event.preventDefault();
+  }
+
+  getFibonacci(position) {
+    axios.get(`${import.meta.env.VITE_REACT_APP_API_SERVICE_URL}/api/fibonacci/${position}`)
+    .then((res) => { this.setState({ fibonacci: res.data["value"], positiveResponseReceived: true }); })
+    .catch((err) => { console.log(err); this.setState({ fibonacci: "An error ocurred!" }); });
+  }
+
+  render() {
+    let result;
+    if (this.state.positiveResponseReceived) {
+      result = <p>Fibonacci Result: {this.state.fibonacci}</p>;
+    } else {
+      result = <p>Submit a number to know its position in the Fibonacci series!</p>;
+    }
+    return (
+        <div className="App">
+          <p>Calculate Fibonacci!</p>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="Calculate" />
+          </form>
+          {result}
+        </div>
+    );
+  }
+}
+
+export default FibonacciForm
--- services/client/src/main.jsx
+++ services/client/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import FibonacciForm from './FibonacciForm'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+{/*    <App />*/}
+    <FibonacciForm />
   </React.StrictMode>
 )
