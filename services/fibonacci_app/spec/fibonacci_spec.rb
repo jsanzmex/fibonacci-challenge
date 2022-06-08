9eabb972a9f0fa6548325841403c80146a86fafb
Julio Sánchez <julio.sanchez@sopristec.com>
2022-06-08 08:46:03 -0500

    + Corrects Rails API URL on React's environment variables.+ Adds context to Rails tests to print a better rspec report.

--- services/fibonacci_app/spec/fibonacci_spec.rb
+++ services/fibonacci_app/spec/fibonacci_spec.rb
@@ -4,26 +4,38 @@ describe 'Fibonacci' do
   subject(:float_fibonacci) { Fibonacci[1.1] }
   subject(:negative_fibonacci) { Fibonacci[-1] }
 
-  it 'is an integer' do
-    expect(valid_fibonacci).to be_an(Integer)
+  context 'given a valid nth term' do
+    it 'is an integer' do
+      expect(valid_fibonacci).to be_an(Integer)
+    end
+
+    it 'returns nth Fibonacci number' do
+      expect(Fibonacci[0]).to eql(0)
+      expect(Fibonacci[1]).to eql(1)
+      expect(Fibonacci[2]).to eql(1)
+      expect(Fibonacci[3]).to eql(2)
+      expect(Fibonacci[4]).to eql(3)
+      expect(Fibonacci[10]).to eql(55)
+      expect(Fibonacci[11]).to eql(89)
+    end
   end
 
-  it 'return -1 when nth term is non and integer' do
-    expect(literal_fibonacci).to eql(-1)
-    expect(float_fibonacci).to eql(-1)
+  context 'given nth term is an invalid literal' do
+    it 'returns -1' do
+      expect(literal_fibonacci).to eql(-1)
+    end
   end
 
-  it 'return -1 when nth term is negative' do
-    expect(negative_fibonacci).to eql(-1)
+  context 'given nth term is an invalid float' do
+    it 'returns -1' do
+      expect(float_fibonacci).to eql(-1)
+    end
   end
 
-  it 'returns the nth Fibonacci number' do
-    expect(Fibonacci[0]).to eql(0)
-    expect(Fibonacci[1]).to eql(1)
-    expect(Fibonacci[2]).to eql(1)
-    expect(Fibonacci[3]).to eql(2)
-    expect(Fibonacci[4]).to eql(3)
-    expect(Fibonacci[10]).to eql(55)
-    expect(Fibonacci[11]).to eql(89)
+  context 'given nth term is valid but negative' do
+    it 'returns -1' do
+      expect(negative_fibonacci).to eql(-1)
+    end
   end
+
 end
